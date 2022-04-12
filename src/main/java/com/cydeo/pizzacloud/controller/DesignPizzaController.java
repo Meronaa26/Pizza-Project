c3bd96eaad233342b4672bc50eb0ab37a170c47b
merry teweldemedhen <meronaa26@gmail.com>
2022-04-11 20:00:19 -0700

    First commit

--- src/main/java/com/cydeo/pizzacloud/controller/DesignPizzaController.java
+++ src/main/java/com/cydeo/pizzacloud/controller/DesignPizzaController.java
@@ -1,23 +1,33 @@
 package com.cydeo.pizzacloud.controller;
 
 import com.cydeo.pizzacloud.bootstrap.DataGenerator;
+import com.cydeo.pizzacloud.enums.Cheese;
+import com.cydeo.pizzacloud.enums.Sauce;
+import com.cydeo.pizzacloud.enums.Topping;
 import com.cydeo.pizzacloud.model.Pizza;
 import com.cydeo.pizzacloud.repository.PizzaRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.UUID;
 
 @Controller
-
+@RequestMapping("/design")
 public class DesignPizzaController {
 
-    private PizzaRepository pizzaRepository;
+   private PizzaRepository pizzaRepository;
 
-    @PostMapping
+    public DesignPizzaController(PizzaRepository pizzaRepository) {
+        this.pizzaRepository = pizzaRepository;
+    }  //injection in order to use the repo class
+
+    @GetMapping()
     public String showDesignForm(Model model) {
-
+        model.addAttribute("pizza",new Pizza());
         model.addAttribute("cheeses", DataGenerator.cheeseTypeList);
         model.addAttribute("sauces", DataGenerator.sauceTypeList);
         model.addAttribute("toppings", DataGenerator.toppingTypeList);
--- src/main/java/com/cydeo/pizzacloud/controller/HomeController.java
+++ src/main/java/com/cydeo/pizzacloud/controller/HomeController.java
@@ -1,8 +1,29 @@
 package com.cydeo.pizzacloud.controller;
 
+import com.cydeo.pizzacloud.bootstrap.DataGenerator;
+import com.cydeo.pizzacloud.enums.Cheese;
+import com.cydeo.pizzacloud.enums.Sauce;
+import com.cydeo.pizzacloud.enums.Topping;
+import com.cydeo.pizzacloud.model.Pizza;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.UUID;
 
 @Controller
+
 public class HomeController {
+
+    @RequestMapping({"/","/home"})
+public String home(){
+
+     return "/home";
+}
+
+
 
 }
